cdafdd1984ce974849802b0a25f091556d111d07
ycervantes <cervantes.y@northeastern.edu>
2021-12-07 15:33:58 -0800

    first commit

---
+++ consts.js
@@ -0,0 +1,7 @@
+const MONGODB_URL = 'mongodb://localhost:27017/sneakpeak';
+// const MONGODB_URL = 'mongodb+srv://web-dev:webdev@cluster0.me8dh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+
+module.exports = {
+  MONGODB_URL
+};
