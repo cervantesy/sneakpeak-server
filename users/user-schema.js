4fe2394cabd3e4c70b16b5a3377d3281446cb8f4
ycervantes <cervantes.y@northeastern.edu>
2021-12-15 16:25:29 -0800

    added avatars

--- users/user-schema.js
+++ users/user-schema.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
   username: String,
   password: String,
+  avatar:String,
   email: String,
   firstName: String,
   lastName: String,
